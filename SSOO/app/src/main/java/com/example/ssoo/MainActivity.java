8046606e836dbaafa1ae276ba78e29fe327b8819
T1907MQuyet <53265406+T1907MQuyet@users.noreply.github.com>
2021-05-26 16:58:55 +0700

    assignment 1

---
+++ SSOO/app/src/main/java/com/example/ssoo/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.ssoo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+//    TextView tvTitle;
+//    EditText edUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+/*
+//        ImageView ivCorver;
+//*/
+////        Button btLogin;
+//        tvTitle = findViewById(R.id.tvtitle);
+//        ImageView ivCorver = findViewById(R.id.ivCorver);
+//        ivCorver.setImageResource(R.drawable.baby_fun);
+//        edUser - findViewById(R.id.edUser);
+//        Button btLogin = findViewById(R.id.btLogin);
+//        btLogin.setOnClickListener(this);
+    }
+//    @Override
+//    public void  onClick(View v){
+//        switch (view.getId()){
+//            case R.id.btLogin:
+//                Log.d("DemoActivity",getUser.getText().toString());
+//                tvTitle.setText("Hello T1907M");
+//                Toast.makeText(MainActivity.this,edUser.getText().toString(),Toast.LENGTH_SHORT).show();
+//
+//                break;
+//            case  R.id.tvTitle:
+//                break;
+//            case
+//        }
+//    }
+}
