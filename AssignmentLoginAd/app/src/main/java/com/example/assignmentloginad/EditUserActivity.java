83f83f7c6af821777d4d8543653cfe08f77ad1a8
T1907MQuyet <53265406+T1907MQuyet@users.noreply.github.com>
2021-05-29 22:35:42 +0700

    upcode login androi mobile

--- AssignmentLoginAd/app/src/main/java/com/example/assignmentloginad/EditUserActivity.java
+++ AssignmentLoginAd/app/src/main/java/com/example/assignmentloginad/EditUserActivity.java
@@ -1,5 +1,6 @@
 package com.example.assignmentloginad;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -17,21 +18,32 @@ public class EditUserActivity extends AppCompatActivity implements View.OnClickL
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-        btDone = (Button)findViewById(R.id.btDone)
-         = (Button) findViewById(R.id.btEdit);
-        btEdit.setOnClickListener(this);
+        setContentView(R.layout.activity_edit_user);
+        edUser = (EditText) findViewById(R.id.edUser);
+        btDone = (Button) findViewById(R.id.btDone);
+        btDone.setOnClickListener(this);
 
         Intent intent = getIntent();
 
-        String username= intent.getStringExtra("USER_NAME");
-
-        tvUser.setText(username);
+        String username = intent.getStringExtra("USER_NAME");
+        edUser.setText(username);
     }
 
 
     @Override
     public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btDone:
+                onDone();
+                break;
+            default:
+                break;
 
+        }
     }
+    private void  onDone(){
+        Intent returnIntent = new Intent();
+        returnIntent.putExtra("RESULT", edUser.getText().toString());
+        setResult(Activity.RESULT_OK,returnIntent);
+        finish();    }
 }
